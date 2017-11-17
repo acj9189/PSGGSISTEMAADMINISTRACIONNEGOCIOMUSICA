d26875b6c40922bbcd968869d47a9a3066ea1f43
acj9189 <anecardenasja@unal.edu.co>
2017-11-17 16:48:50 -0500

    Actualizaciones
    para todos menos caro este link les va a servir mucho...
    
    https://gist.github.com/hootlex/da59b91c628a6688ceb1

--- Programacion/Audiopro/routes/web.php
+++ Programacion/Audiopro/routes/web.php
@@ -9,7 +9,15 @@
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
+
+
+Route::group('',function(){
+
+});*/
+
+Route::get('/', function(){
+	return view('welcome');
+});
 
 Route::get('EntradaArticulo', function () {
     return view('paginas.EntradaArticulo');
