164fc35ddcbc3dc6366017b4aa95e26ad408d55c
santix96 <32211963+santix96@users.noreply.github.com>
2017-11-29 01:10:30 -0500

    modificacion base de datos

--- Programacion/Audiopro/database/migrations/2017_11_18_092117_migracionArticulo.php
+++ Programacion/Audiopro/database/migrations/2017_11_18_092117_migracionArticulo.php
@@ -5,7 +5,7 @@ use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
 class MigracionArticulo extends Migration
-{
+{   
     /**
      * Run the migrations.
      *
@@ -15,17 +15,17 @@ class MigracionArticulo extends Migration
     {
         Schema::create('tArticulo', function (Blueprint $table) {
 
-            $table->increments('idA');
+           $table->increments('idA');
             //$table->integer('id')->length(100)->unsigned();
             $table->string('numero_Serie', 100);
             $table->string('marca', 100);
             $table->string('modelo', 10);
             $table->string('estado', 10);
-            $table->string('descripcion', 100);
-            $table->string('problema_reportado', 100);
+            $table->string('descripcion', 300)->nullable();
+            $table->string('problema_reportado', 300);
             $table->string('tipo_Servicio',10);
             $table->date('fecha_Ingreso');
-            $table->date('fecha_Salida');
+            $table->date('fecha_Salida')->nullable();
             //$table->primary('id');
             $table->timestamps();
 
